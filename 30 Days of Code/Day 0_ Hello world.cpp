42d1ea436a0db80baa9984b9d7fa73a74ea29d9e
Mrinal Pramanik <mrinalpramanik98@gmail.com>
2020-05-07 13:01:09 +0530

    C++ code

---
+++ 30 Days of Code/Day 0_ Hello world.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    char str[100];
+    cin.getline(str,100);
+    cout<<"Hello, World.";
+    cout<<"\n"<<str;
+}
+
+
