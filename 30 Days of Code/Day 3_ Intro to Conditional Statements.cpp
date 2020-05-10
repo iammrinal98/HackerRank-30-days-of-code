d7a82002da1f085dcb590e43ea5b254f0ebe8dcd
Mrinal Pramanik <mrinalpramanik98@gmail.com>
2020-05-10 11:13:36 +0530

    C++ code

---
+++ 30 Days of Code/Day 3_ Intro to Conditional Statements.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+      int n;
+      cin>>n;
+      if(n%2==0){
+          if(n>=2 && n<=5)
+          cout<<"Not Weird";
+          else if(n>=6 && n<=20)
+          cout<<"Weird";
+          else if(n>20)
+          cout<<"Not Weird";
+      }
+      else
+      cout<<"Weird";
+    return 0;
+}
