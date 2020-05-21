def7dce82128edbe9b3cde9e495e7df8d014a024
Mrinal Pramanik <mrinalpramanik98@gmail.com>
2020-05-21 11:59:50 +0530

    C++ code

---
+++ 30 Days of Code/Day 14_ Scope.cpp
@@ -0,0 +1,48 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	// Add your code here
+    Difference(vector<int> newElements) {
+    elements = newElements;
+    sort(elements.begin(),elements.end());
+  }
+
+  void computeDifference() {
+    maximumDifference = elements[elements.size()-1] - elements[0];
+  }
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
